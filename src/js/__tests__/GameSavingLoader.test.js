0e7c1954dae789dfe4c37125a04456227314280f
Alla Kruglova <renata-l@yandex.ru>
2021-08-07 20:23:47 +0300

    first commit

---
+++ src/js/__tests__/GameSavingLoader.test.js
@@ -0,0 +1,6 @@
+
+import GameSavingLoader from '../GameSavingLoader';
+
+test('работа класса', async () => {
+  expect(await GameSavingLoader.load()).toEqual('{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}');
+});
